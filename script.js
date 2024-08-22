6d783d0298d97ff2a3d749c2b0590a5a8ff5e08b
noobmaster434 <72881779+noobmaster434@users.noreply.github.com>
2024-08-22 13:17:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function showYesMessage() {
     document.getElementById('response').textContent = 'I knew you would say yes, wait for more updates. Oh sorry! I dont want to keep you waiting, meet me at the Airport';
-    document.body.style.backgroundImage = "url('kalra/bg2.png')"; // Replace with your second background image URL
+    document.body.style.backgroundImage = "url('bg2.png')"; // Replace with your second background image URL
 }
 
 function moveButton() {
